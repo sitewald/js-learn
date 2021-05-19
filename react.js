7d376420e77d956e30f9a89d0a9b958c89763f88
Белозеров Сергей <belozyorovsergey@yandex.ru>
2021-05-19 16:19:53 +0300

    add react

---
+++ react.js
@@ -0,0 +1,8 @@
+
+	// ---------------- REACT JS
+
+	// React JS - UI-библиотека а не фреймворк (opensource от Facebook)
+	
+	// UI-компоненты
+	
+	// Reconciliation algorithm - перерисовываются только изменённые места
